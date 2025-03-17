7ac26ccb975e89ef49630c46fe08b0a22c47231b
makifcevik <makif.cevik02@gmail.com>
2025-03-17 06:45:35 +0300

    Add Product test data

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -3,12 +3,70 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class DataContext : DbContext
+public class DataContext(DbContextOptions options) : DbContext(options)
 {
-    public DataContext(DbContextOptions options) : base(options)
-    {
-
-    }
-
     public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder
+            .Entity<Product>()
+            .HasData(
+                new List<Product>
+                {
+                    new Product
+                    {
+                        Id = 1,
+                        Name = "Iphone 15",
+                        Description = "Phone description",
+                        ImageUrl = "1.jpg",
+                        IsActive = true,
+                        Price = 40000,
+                        Stock = 120,
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        Name = "Iphone 16",
+                        Description = "Phone description",
+                        ImageUrl = "2.jpg",
+                        IsActive = false,
+                        Price = 50000,
+                        Stock = 95,
+                    },
+                    new Product
+                    {
+                        Id = 3,
+                        Name = "Iphone 17",
+                        Description = "Phone description",
+                        ImageUrl = "3.jpg",
+                        IsActive = true,
+                        Price = 70000,
+                        Stock = 80,
+                    },
+                    new Product
+                    {
+                        Id = 4,
+                        Name = "Iphone 18",
+                        Description = "Phone description",
+                        ImageUrl = "4.jpg",
+                        IsActive = true,
+                        Price = 100000,
+                        Stock = 250,
+                    },
+                    new Product
+                    {
+                        Id = 5,
+                        Name = "Iphone 19",
+                        Description = "Phone description",
+                        ImageUrl = "5.jpg",
+                        IsActive = true,
+                        Price = 150000,
+                        Stock = 20,
+                    },
+                }
+            );
+    }
 }
---
+++ API/Migrations/20250317034324_TestData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "IsActive", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 1, "Phone description", "1.jpg", true, "Iphone 15", 40000m, 120 },
+                    { 2, "Phone description", "2.jpg", false, "Iphone 16", 50000m, 95 },
+                    { 3, "Phone description", "3.jpg", true, "Iphone 17", 70000m, 80 },
+                    { 4, "Phone description", "4.jpg", true, "Iphone 18", 100000m, 250 },
+                    { 5, "Phone description", "5.jpg", true, "Iphone 19", 150000m, 20 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
